39c0652f166e7dfaf9f2b8940316f1e8b5119807
butane-in-vein <gabriel.aglen.brindlergmail.com>
2025-01-11 15:49:03 +0100

    added custom fonts, added the block and Mino class to commence creating the 7 tetraminos in the game

--- src/main/PlayManager.java
+++ src/main/PlayManager.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class PlayManager {
 
@@ -11,6 +13,7 @@ public class PlayManager {
   public static int right_x;
   public static int top_y;
   public static int bottom_y;
+  Font maruMonica;
 
   public PlayManager() {
 
@@ -20,23 +23,40 @@ public class PlayManager {
     top_y = 50;
     bottom_y = top_y + HEIGHT;
 
+    try { // ADDS CUSTOM FONT
+      InputStream is = getClass().getClassLoader().getResourceAsStream("font/x12y16pxMaruMonica.ttf");
+      maruMonica = Font.createFont(Font.TRUETYPE_FONT, is);
+    } catch (FontFormatException e) {
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
   }
+
   public void update() {
 
   }
+
   public void draw(Graphics2D g2) {
 
     // DRAW PLAY AREA FRAME
     g2.setColor(Color.white); // SETS THE COLOR OF THE PLAY AREA FRAME
     g2.setStroke(new BasicStroke(4f)); // SETS THE THICKNESS OF THE STROKE
-    g2.drawRect(left_x-4, top_y-4,WIDTH+8, HEIGHT+8); // DRAWS THE RECTANGLE OF OUR PLAY AREA, USING ABOVE METHODS FOR SIZE AND COLOR
+    g2.drawRect(left_x - 4, top_y - 4, WIDTH + 8, HEIGHT + 8); // DRAWS THE RECTANGLE OF OUR PLAY AREA, USING ABOVE METHODS FOR SIZE AND COLOR
 
     // DRAW FRAME FOR UPCOMING BLOCKS
     int x = right_x + 100; // Moves to the right along the x axis, based on the right_x placement
     int y = bottom_y - 200; // moves up along the y axis, based on the bottom_y placement
     g2.drawRect(x, y, 200, 200);
-    g2.setFont(new Font("Arial", Font.PLAIN, 30)); // SETS THE FONT USED, MIGHT USE THE 8BIT TEXT l8R
+
+
+//    g2.setFont(new Font("Arial", Font.PLAIN, 30)); // SETS THE FONT USED, MIGHT USE THE 8BIT TEXT l8R
+    g2.setFont(maruMonica); // SETS THE FONT, WE USE A CUSTOM 8BIT ONE
+    g2.setFont(g2.getFont().deriveFont(Font.BOLD, 30));
     g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-    g2.drawString("NEXT", x+60, y+60); // DRAWS "NEXT" INSIDE THE BOX
+    g2.drawString("NEXT", x + 60, y + 40); // DRAWS "NEXT" INSIDE THE BOX
+
+
   }
 }
